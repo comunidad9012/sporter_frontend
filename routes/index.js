379402f8a1a0972647e79ca24e74e56f3f443466
German Rinaldi <110374480+ger-rinaldi@users.noreply.github.com>
2024-04-03 22:47:06 -0300

    feat: agregar ruta para formulario actualizacion

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('catalogo', { requestURL: process.env.REQUEST_URL });
+router.get("/", function (req, res, next) {
+  res.render("catalogo", { requestURL: process.env.REQUEST_URL });
+});
+
+router.get("/actualizar/:productID", function (req, res, next) {
+  res.render("editarProducto", { productID: req.params.productID });
 });
 
 module.exports = router;
