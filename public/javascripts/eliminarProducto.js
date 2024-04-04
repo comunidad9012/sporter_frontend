e7c8efc48cb341f2d1cb41e5a10502c5879b9086
German Rinaldi <110374480+ger-rinaldi@users.noreply.github.com>
2024-04-04 14:20:48 -0300

    feat: integrar boton eliminar producto

---
+++ public/javascripts/eliminarProducto.js
@@ -0,0 +1,33 @@
+import { requestURL } from "/static/javascripts/catalogRequestHelpers.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const confirmDeleteBtn = document.getElementById("confirmDeleteBtn");
+  const cancelDeleteBtn = document.getElementById("cancelDeleteBtn");
+
+  confirmDeleteBtn.addEventListener("click", async function () {
+    // Me falta la obtencion del id del producto, esto es de prueba
+    const productId =  window.location.href.split('/').reverse()[0];
+
+    try {
+      const response = await fetch(
+          requestURL + "/eliminar/" + productId,
+        {
+          method: "POST",
+        }
+      );
+
+      if (response.ok) {
+          window.location.href = "/";
+      } else {
+        const errorData = await response.json();
+        console.error("Error al eliminar el producto:", errorData.error);
+      }
+    } catch (error) {
+      console.error("Error en la solicitud:", error);
+    }
+  });
+  // botón de cancelar
+  cancelDeleteBtn.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
+});
