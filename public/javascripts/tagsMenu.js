ae23f43ebf39f37d5a00169ec69372fd03618bef
German Rinaldi <110374480+ger-rinaldi@users.noreply.github.com>
2024-04-17 21:03:04 -0300

    refactor: actualizar consultas y render de catalogo en tagsMenu
    Se aplicaron los objetos de interfaz de api y funciones de renderizado
    en tagsMenu para actualizar el funcionamiento previo de requests

--- public/javascripts/tagsMenu.js
+++ public/javascripts/tagsMenu.js
@@ -1,35 +1,40 @@
-import { APIEtiquetaURL, APIProductURL } from "/static/javascripts/catalogRequestHelpers.js";
+import { etiquetaApi, productoApi } from "/static/classes/resources.js";
+import { populateCatalog } from "/static/javascripts/productsUI.js";
+import { renderPageButtons } from "/static/javascripts/pagination.js";
+
+function tagButtonQuery(tagButton, tagName) {
+  tagButton.addEventListener("click", async function () {
+    await productoApi.buscarPorEtiqueta(tagName).then((data) => {
+      populateCatalog(data);
+      renderPageButtons(data);
+    });
+  });
+}
 
 // Función para hacer la solicitud a la API y poblar la lista
-async function populateList(requestURL) {
-  const response = await fetch(requestURL);
-  const data = await response.json();
+async function populateList() {
+  const data = await etiquetaApi.readAll();
 
   const tagList = document.getElementById("tag-list");
 
   tagList.innerHTML = "";
 
   // Iteramos sobre los datos y creamos elementos de lista para la lista
-  data.forEach(tag => {
-    const divBotones = document.querySelector('#botones');
+  data.forEach((tag) => {
+    const divBotones = document.querySelector("#botones");
     const listItem = document.createElement("li");
     listItem.classList.add("tag-list-item");
     listItem.innerHTML = `
       <button class="tag-button" data-tag="${tag.nombre}">${tag.nombre} (${tag.total_productos})</button>
     `;
+
+    tagButtonQuery(listItem, tag.nombre);
     tagList.appendChild(listItem);
     divBotones.appendChild(tagList);
   });
 
   // Agregar un event listener a cada botón para manejar el clic
-  document.querySelectorAll(".tag-button").forEach(button => {
-    button.addEventListener("click", () => {
-      const tagName = button.getAttribute("data-tag");
-      // Llamar a la función para filtrar los productos relacionados con el tag
-      filterProducts(tagName);
-    });
-  });
 }
 
 // Llamamos a la función para poblar la lista al cargar la página
-window.onload = () => populateList(APIEtiquetaURL + "/leer/");
+window.onload = () => populateList();
