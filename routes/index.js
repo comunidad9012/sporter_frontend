39a8455d7ac3f196fa22feecc9c050c5db5acf9a
German Rinaldi <110374480+ger-rinaldi@users.noreply.github.com>
2024-04-25 01:45:24 -0300

    restringir accesso a acciones a no admins

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,40 @@
 var express = require("express");
 var router = express.Router();
 
+function isPrivileged(cookies){
+
+  if ( !Number(cookies.logged_in) || !Number(cookies.is_admin)){
+      return false
+  }
+  return true
+
+}
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("catalogo", { APIBaseURL: process.env.REQUEST_URL });
 });
 
 router.get("/actualizar/:productID", function (req, res, next) {
-  res.render("editarProducto", {
-    productID: req.params.productID,
-    APIBaseURL: process.env.REQUEST_URL,
-  });
+  if (isPrivileged(req.cookies)){
+    res.render("editarProducto", {
+      productID: req.params.productID,
+      APIBaseURL: process.env.REQUEST_URL,
+    });
+  } else{
+    res.redirect("/not_allowed")
+  }
 });
 
 // Registro
 router.get("/register", function (req, res, next) {
-  res.render("register", {
-    APIBaseURL: process.env.REQUEST_URL,
-  });
+  if (isPrivileged(req.cookies)){
+    res.render("register", {
+      APIBaseURL: process.env.REQUEST_URL,
+    });
+  } else{
+    res.redirect("/not_allowed")
+  }
 });
 
 // Inicio de sesion
@@ -29,9 +46,17 @@ router.get("/signIn", function (req, res, next) {
 
 // Administrar usuarios
 router.get("/administration", function (req, res, next) {
-  res.render("administration",{
-    APIBaseURL: process.env.REQUEST_URL,
-  });
+  if (isPrivileged(req.cookies)){
+    res.render("administration",{
+      APIBaseURL: process.env.REQUEST_URL,
+    });
+  } else{
+    res.redirect("/not_allowed")
+  }
 });
+
+router.get("/not_allowed", function (req, res, next) {
+  res.render("not_allowed")
+})
 
 module.exports = router;
