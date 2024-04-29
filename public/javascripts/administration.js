17894910a0073d35b409a95e34e0f875e6fedbf2
German Rinaldi <110374480+ger-rinaldi@users.noreply.github.com>
2024-04-29 14:58:35 -0300

    fix: permitir a administradores dar privilegios en tabla de usuarios

--- public/javascripts/administration.js
+++ public/javascripts/administration.js
@@ -14,7 +14,7 @@ if (window.location.pathname === '/administration') {
                     <td>${user.usuario}</td>
                     <td>${user.correo}</td>
                     <td>
-                        <button onclick="openModal('${user.nombre}', '${user.correo}')">Modificar</button>
+                        <button onclick="openModal('${user.nombre}', '${user.correo}', '${user.is_admin}', '${user.usuario}')">Modificar</button>
                         <button onclick="deleteUser(${user.id})">Eliminar</button>
                     </td>
                 `;
@@ -27,11 +27,14 @@ if (window.location.pathname === '/administration') {
 
 
 // Logica para abrir el modal con los datos del usuario seleccionado para editar
-function openModal(nombre, correo) {
+function openModal(nombre, correo, is_admin, username) {
     const editNombre = document.getElementById('editNombre');
     const editCorreo = document.getElementById('editCorreo');
+    const editIsAdmin = document.getElementById("editIsAdmin");
+    
     editNombre.value = nombre;
     editCorreo.value = correo;
+    editIsAdmin.checked = is_admin == "true" ? 1 : 0;
     
     const modal = new bootstrap.Modal(document.getElementById('editModal'));
     modal.show();
@@ -44,11 +47,14 @@ function openModal(nombre, correo) {
         // Obtiene los valores actualizados de los campos de nombre y correo
         const nuevoNombre = editNombre.value;
         const nuevoCorreo = editCorreo.value;
+        const nuevoIsAdmin = editIsAdmin.checked ? 1 : 0;
 
         // Crea un objeto FormData y agrega los campos de nombre y correo
         const formData = new FormData();
         formData.append('nombre', nuevoNombre);
         formData.append('correo', nuevoCorreo);
+        formData.append('is_admin', nuevoIsAdmin);
+        formData.append("usuario", username)
 
         // Envía los datos de edición al servidor utilizando FormData
         fetch( document.getElementById("APIBaseURL").value + 'api/user/actualizar', {
